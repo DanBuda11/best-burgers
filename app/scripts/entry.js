c02a14ecf2ef53c1a1316e2778364298bc4d1f08
anna hunter <annfmonty@gmail.com>
2016-04-08 15:17:13 -0500

    connectivity test

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -3,3 +3,16 @@ import './../styles/main.scss';
 import $ from 'jquery';
 import BurgerCollection from './collections/burgerCollection';
 
+
+
+let potato = new BurgerCollection();
+var settings = {
+	success: function() {
+		potato.forEach((test) => {
+			console.log(test.get('title'));
+
+		})
+	}
+};
+			
+potato.fetch(settings);
---
+++ app/views/newBurgerSubmission.js
@@ -0,0 +1,4 @@
+import Backbone from 'backbone';
+import './../styles/main.scss';
+import $ from 'jquery';
+import BurgerCollection from './collections/burgerCollection';
