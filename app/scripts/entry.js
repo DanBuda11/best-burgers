d231e013d33488f3d70f87c9afecbc81b9111266
anna hunter <annfmonty@gmail.com>
2016-04-08 13:51:26 -0500

    new file structure

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -1,8 +1,5 @@
-// import the stylesheet. this is necessary so that webpack will compile all the sass into css and then build it into our style.css file
+import Backbone from 'backbone';
 import './../styles/main.scss';
+import $ from 'jquery';
+import BurgerCollection from './collections/burgerCollection';
 
-// import a module from another file.
-import tiy from './app.js';
-
-// Looks like the imported module was a function, because here we're executing it!
-tiy();
