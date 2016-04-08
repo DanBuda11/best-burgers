6cf9538484e99858b7ce40ad8c7663d6502ee038
Dan Buda <danbuda@gmail.com>
2016-04-08 16:36:46 -0500

    burgerview works with static language

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -1,8 +1,38 @@
 import Backbone from 'backbone';
 import './../styles/main.scss';
 import $ from 'jquery';
-import BurgerView from '../viewss/BurgerView';
+import BurgerView from './views/BurgerView.js';
 
 let newBurger = new BurgerView();
 newBurger.render();
 $('body').append(newBurger.$el);
+
+
+
+
+
+// change all the below to match burgers not polarBears
+
+var settings = {
+	success: function() {
+		polarBears.forEach((image) => {
+			console.log(image.get('caption'));
+			let newPolarBear = new ImageView(image.get('caption'), image.get('src'));
+			$('.image-list').append(newPolarBearView.$el);
+		});
+	}
+};
+
+polarBears.fetch(settings);
+
+$('.image-form').on('submit', (e) => {
+	e.preventDefault();
+	let new PolarBear = {
+		src: $('.src').val(),
+		caption: $('.caption').val()
+	};
+	polarBears.create(newPolarBear);
+});
+
+
+let polarBears = new ImageCollection();
--- app/scripts/views/BurgerView.js
+++ app/scripts/views/BurgerView.js
@@ -1,16 +1,29 @@
 import Backbone from 'backbone';
 import $ from 'jquery';
-import BurgerModel from '../models/BurgerModel';
+import BurgerModel from '../models/burgerModel';
+import BurgerCollection from '../collections/burgerCollection';
 
-export default BackBone.View.extend({
+export default Backbone.View.extend({
 	className: 'burgerBox',
-	initialize: function(argument) {
-		this. = argument;
+	initialize: function(newBurger) {
+		this.item = newBurger;
 		this.render();
 	},
 	render: function() {
 		const template = `
-			<div></div>`;
-			this.$el.html(template);
+			<div class="col col-8 burger-container">
+				<div class="burger-picture">
+				</div>
+				<div class="burger-info">
+					<div class="burger-name">
+						<h2>The American Burger</h2>
+						<h3>Uncle Willy's Tavern</h3>
+					</div>	
+					<div class="voting-and-likes">
+						<i class="fa fa-thumbs-up"></i>(# of likes)
+					</div>
+				</div>
+			</div>`;
+		this.$el.html(template);
 	}
 });
