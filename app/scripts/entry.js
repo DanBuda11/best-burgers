dcfef555859261239e7af72d8b6f832ee5231215
anna hunter <annfmonty@gmail.com>
2016-04-08 20:14:36 -0500

    new HTML and input form code

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -5,19 +5,58 @@ import BurgerView from '../viewss/BurgerView';
 
 
 
-let potato = new BurgerCollection();
-var settings = {
-	success: function() {
-		potato.forEach((test) => {
-			console.log(test.get('title'));
+$( ".addBurger" ).click(function () {
+  if ( $( ".input-form" ).is( ":hidden" ) ) {
+    $( ".input-form" ).slideDown( "slow" );
+  } else {
+    $( ".input-form" ).slideUp();
+  }
+});
 
-		})
-	}
-};
+$(".input-form").submit (e) => {
+	e.preventDefault;
+		let submitBurger = {
+		title: $('.burgerTitle').val();
+		location: $('.burgerLoc').val();
+		description: $('.burgerDesc').val();
+		pic: $('.burgerPic').val();
+	};
+	post.create(newBurger);
+	let newBurger = new BurgerView(
+		newBurger.location;
+		newBurger.description;
+		newBurger.pic;
+	);
+	$('body').append(newTBurger.$el);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let potato = new BurgerCollection();
+// var settings = {
+// 	success: function() {
+// 		potato.forEach((test) => {
+// 			console.log(test.get('title'));
+
+// 		})
+// 	}
+// };
 			
-potato.fetch(settings);
+// potato.fetch(settings);
 
-let newBurger = new BurgerView();
-newBurger.render();
-$('body').append(newBurger.$el);
+// let newBurger = new BurgerView();
+// newBurger.render();
+// $('body').append(newBurger.$el);
 
